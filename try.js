b68da40f60556ad84447f8459a1ae69acbe37352
anoop <anoop.kulkarni@gmail.com>
2013-04-15 16:33:56 -0500

    loop through jobs

--- try.js
+++ try.js
@@ -1,15 +1,17 @@
 var nodejobs = require('node-cron-jobs');
 var jobs = nodejobs.jobs;
-var func = function(){
-	console.log('print this every time the job runs');
+var cronFunc = function(jobName){
+  console.log(jobName);
+  var func = function(){
+        console.log("print this every time job "+jobName+" runs");
+  }
+  jobs[jobName].addCallback(func);
+  jobs[jobName].start();
+};
+for(var i in jobs){
+  cronFunc(i);
 }
-jobs.firstjob.addCallback(func);
-jobs.firstjob.start();
+
 console.log(nodejobs.config.dburl);
 console.log(jobs.firstjob.config.url);
 console.log(jobs.firstjob.config.time);
-console.log('**********');
-var func1 = function(){
-	console.log('print for second job');
-}
-jobs.secondjob.addCallback(func1);
