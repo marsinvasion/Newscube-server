873d498aef6060ab9084cafb00599c25f22e5210
anoop <anoop.kulkarni@gmail.com>
2013-04-25 17:29:49 -0500

    Add rss parser

--- rss.js
+++ rss.js
@@ -1,6 +1,24 @@
-var parser = require('rssparser');
-var options = {};
-//rss feeds
-parser.parseURL('http://rssfeeds.usatoday.com/usatoday-NewsTopStories', options, function(err, out){
-    console.log(out);
-});
+var nodejobs = require('node-cron-jobs');
+var jobs = nodejobs.jobs;
+var date = new Date();
+var cronFunc = function(jobName, config){
+  var job = jobs[jobName]; 
+  console.log("Starting", jobName, date);
+  console.log("callback", job.onTick);
+  function parse(){
+	console.log("kicking off job");
+	var parser = require('rssparser');
+	parser.parseURL(config.url, {}, function(err, out){
+	    console.log(out);
+	});
+  }
+  console.log("callbacks", job._callbacks.length);
+  job.addCallback(parse);
+  job.start();
+  console.log("callbacks", job._callbacks.length);
+  console.log(job.cronTime.getTimeout(), "next fire", jobName);
+};
+for(var i in jobs){
+  cronFunc(i, jobs[i].config);
+}
+
