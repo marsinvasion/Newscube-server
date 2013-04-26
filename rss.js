140d9582ea0af0a82a2281b16abe66b02638ad29
anoop <anoop.kulkarni@gmail.com>
2013-04-26 17:35:15 -0500

    Change data structure to save by date

--- rss.js
+++ rss.js
@@ -13,29 +13,33 @@ var cronFunc = function(jobName, config){
   var parser = require('rssparser');
   console.log("Starting", jobName, date);
   var parse = function (){
-	console.log("kicking off job");
+	console.log("kicking off job", jobName);
 	parser.parseURL(config.url, {}, function(err, out){
 	    for(var j in out.items){
 		(function (j) {
 		var item = out.items[j];
-		var hash = jobName + ":" + item.url;
-		client.hexists(hash, "title", function (err, reply) {
-		  debugger;
-        	  if(reply == 0){
-		    console.log("Saving to db", hash);
-		    var date = new Date(item.published_at);
-		    client.HMSET(hash,{
+		var today = new Date();
+		var dd = today.getDate();
+		var mm = today.getMonth()+1; //January is 0!
+		var yyyy = today.getFullYear();
+		if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = mm+'/'+dd+'/'+yyyy;
+		var key = config.country+":"+today;
+		client.sadd("country", key);
+		client.sadd(key, item.url, function (error, reply){
+		debugger;
+		if(reply == 1){ //url did not exist in db, save feed
+		    var published = new Date(item.published_at);
+		    client.HMSET(item.url,{
 			"title":item.title,
 			"summary":item.summary,
-			"url":item.url,
-			"published_at":date
+			"published_at":published,
+			"inserted_at":date
 		   } );
-		  }else{
-			console.log("Already exists in db", hash);
-		  }
-    		});
+		   console.log("Saved", key, item.url);
+    		}
+		});
 	    }) (j);
-	}
+	  }
 	});
   }
   job.addCallback(parse);
