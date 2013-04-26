bb333060f7b56a70d453f8d3df2b6d0828435b6b
anoop <anoop.kulkarni@gmail.com>
2013-04-25 19:14:54 -0500

    Add rss parser

--- rss.js
+++ rss.js
@@ -1,22 +1,45 @@
 var nodejobs = require('node-cron-jobs');
 var jobs = nodejobs.jobs;
 var date = new Date();
+var redis = require("redis"),
+        client = redis.createClient();
+
+    client.on("error", function (err) {
+        console.log("Redis Error " + err);
+    });
+
 var cronFunc = function(jobName, config){
   var job = jobs[jobName]; 
+  var parser = require('rssparser');
   console.log("Starting", jobName, date);
-  console.log("callback", job.onTick);
-  function parse(){
+  var parse = function (){
 	console.log("kicking off job");
-	var parser = require('rssparser');
 	parser.parseURL(config.url, {}, function(err, out){
-	    console.log(out);
+	    for(var j in out.items){
+		(function (j) {
+		var item = out.items[j];
+		var hash = jobName + ":" + item.url;
+		client.hexists(hash, "title", function (err, reply) {
+		  debugger;
+        	  if(reply == 0){
+		    console.log("Saving to db", hash);
+		    var date = new Date(item.published_at);
+		    client.HMSET(hash,{
+			"title":item.title,
+			"summary":item.summary,
+			"url":item.url,
+			"published_at":date
+		   } );
+		  }else{
+			console.log("Already exists in db", hash);
+		  }
+    		});
+	    }) (j);
+	}
 	});
   }
-  console.log("callbacks", job._callbacks.length);
   job.addCallback(parse);
   job.start();
-  console.log("callbacks", job._callbacks.length);
-  console.log(job.cronTime.getTimeout(), "next fire", jobName);
 };
 for(var i in jobs){
   cronFunc(i, jobs[i].config);
